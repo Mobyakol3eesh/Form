4e72cc4560c6f7e56a013bcfca2a883bd7ecde0a
mobyakol3eesh <mohamed_hesham203@icloud.com>
2024-08-23 16:21:38 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let emailInput = document.getElementById("email-input");
+let fnameInput = documnet.getElementById("fname-input");
+let lnameInput = documnet.getElementById("lname-input");
+let passInput = document.getElementById("pass-input");
+let mobInput = documnet.getElementById("mob-input");
+let submitBtn = documnet.getElementById("submit-btn");
+
+let isEmailValid = false;
+let isFnameValid = false;
+let isLnameValid = false;
+let isPassValid = false;
+let isMobValid = false;
+
+
+
+
+
+
+
+
